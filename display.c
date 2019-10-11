d9dda9379da4f62dec88e9dc048f5c737e5689d8
Julian Alexander Topp <jat191@cs18258kq.canterbury.ac.nz>
2019-10-11 15:09:19 +1300

    ready for testing

--- display.c
+++ display.c
@@ -1,11 +1,16 @@
 #include "system.h"
-#include "pacer.h"
-#include "navswitch.h"
 #include "tinygl.h"
 #include "../fonts/font5x7_1.h"
 
 #define PACER_RATE 500
+#define LOOP_RATE 500
 
 void display_character(char character)
 {
-    tinygl_init();
+    tinygl_init(LOOP_RATE);
+    tinygl_font_set(&font5x7_1);
+    char buffer[2];
+    buffer[0] = character;
+    buffer[1] = '\0';
+    tinygl_text(buffer);
+}
