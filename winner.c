aae71a8f6a777dc96c528957be5fd97fee219309
Sean Panashe Madondo <sma297@cs17092jp.canterbury.ac.nz>
2019-10-14 14:29:22 +1300

    Updates to comments and prechecks

--- winner.c
+++ winner.c
@@ -1,17 +1,21 @@
-/* Winner Function to decide who won */
+/** @file winner.c
+ * @author Julian Topp, Sean Madondo
+ * @date 14/0/2019
+ * @brief Decide winner
+ */
 
+/* Function to decide winner of game
+ * Takes two characters, follows truth table to decide winner
+ * Return 0 if it is a draw
+ * Return 1 if player1 wins
+ * Return 2 if player2 wins */
 
 int winner(char player1, char player2)
 {
-
-    /* Return 0 if it is a draw
-     * Return 1 if player1 wins
-     * Return 2 if player2 wins */
-
     int win = 0;
 
     if (player1 == player2) {
-        win = 0;
+        win = 0; //if it is a draw
     } else if (player1 == 'P' && player2 == 'R') {
         win = 1;
     } else if (player1 == 'R' && player2 == 'S') {
--- winner.h
+++ winner.h
@@ -1,6 +1,13 @@
+/** @file winner.h
+ * @author Julian Topp, Sean Madondo
+ * @date 14/0/2019
+ * @brief Decide game winner
+ */
+
 #ifndef WINNER_H_
 #define WINNER_H_
 
+//Conditionals for winner decision
 int winner(char player1, char player2);
 
 #endif
